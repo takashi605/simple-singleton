74df1971928f61590efe7c1a3b07d97622dba734
takashi605 <tetocir@gmail.com>
2025-08-16 10:59:08 +0900

    結合テストをリファクタリング

--- application/tests/singleton_works.rs
+++ application/tests/singleton_works.rs
@@ -2,29 +2,29 @@ use simple_singleton::SingletonCounter;
 
 #[test]
 fn counters_are_pointing_to_the_same_instance() {
+    // SingletonCounter を2回取得して、同じインスタンスを指していることを確認
     let counter1 = SingletonCounter::get();
     let counter2 = SingletonCounter::get();
+
     assert!(std::ptr::eq(counter1, counter2));
 }
 
 #[test]
 fn count_state_is_shared() {
     {
-        let counter = SingletonCounter::get().lock().unwrap();
-        let count = counter.count;
-        assert_eq!(count, 0);
-    }
-
-    {
         let mut counter = SingletonCounter::get().lock().unwrap();
+
+        // SingletonCounter を取得して、初期状態の count が 0 であることを確認
+        assert_eq!(counter.count, 0);
+
+        // SingletonCounter の count を 1 増やす
         counter.add(1);
-        let count = counter.count;
-        assert_eq!(count, 1);
+        assert_eq!(counter.count, 1);
     }
 
+    // 再度 SingletonCounter を取得して、count が 1 であることを確認
     {
         let counter = SingletonCounter::get().lock().unwrap();
-        let count = counter.count;
-        assert_eq!(count, 1);
+        assert_eq!(counter.count, 1);
     }
 }
